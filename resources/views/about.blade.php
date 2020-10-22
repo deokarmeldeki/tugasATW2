3d612d08034435f4d4b21e8e14c39d79a2adc73c
deokarmeldeki <dekisiansahan64@gmail.com>
2020-10-22 05:31:50 -0500

    menambahkan form about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,134 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>about</title>
-</head>
-<body>
-	<h2>ABOUT</h2>
-</body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css" integrity="sha384-SI27wrMjH3ZZ89r4o+fGIJtnzkAnFs3E4qz9DIYioCQ5l9Rd/7UAa8DHcaL8jkWt" crossorigin="anonymous">
+    <link   rel="stylesheet" href="assets/font-awesome/less/font-awesome.css">
+    <link   rel="stylesheet" href="assets/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>about</title>
+  </head>
+  <body>
+    <style >
+body{
+            margin: 0;
+            padding: 0;
+        }
+        body:before{
+            content: '';
+            position: fixed;
+            width: 100vw;
+            height: 100vh;
+            background-image: url("css/images/bcgr.jpg");
+            background-position: center center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }>    </style>
+        <nav class="navbar navbar-expand-lg navbar-light bg-info fixed-top">
+            <div class="container">
+                <h3><i class="fa fa-home mr-2"></i></h3>
+            <a class="navbar-brand font-weight-bold" href="#">Siansahan 64</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto mr-4">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="/layout">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item active">
+                    <a class="nav-link" href="admin.php">Data <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item active">
+                    <a class="nav-link" href="tentang_kami.html">Tentang Kami <span class="sr-only">(current)</span></a>
+                  </li>
+                </ul>
+                
+              </div>
+            </div>
+       </nav>
+       <div class="container mt-5 pt-4 text-white">
+        <div class="row text-white">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-6 mr-5">
+                        <img style="width:1140px;height:450px;" st="" src="css/images/s64.png" alt="">
+                    </div>
+                    <div class="col-md-8 text-white">
+                        <h1>Deo Karmel Deki</h1>
+                        <h2>304 2018 281</h2>
+                        <table class="table text-white">
+                            <thead>
+                                <th width="40%"></th>
+                                <th></th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>Nama Lengkap</th>
+                                    <td>Deo Karmel Deki</td>
+                                </tr>
+                                <tr>
+                                    <th><i class="fa fa-home"></i>  Tempat, Tanggal Lahir</th>
+                                    <td>Batu Bulan, 03 Februari 2000</td>
+                                </tr>
+                                <tr>
+                                    <th><i class="fa fa-envelope">  E-mail</i></th>
+                                    <td>dekisiansahan64@gmail.com</td>
+                                </tr>
+                                <tr>
+                                    <th><i class="fa fa-whatsapp"> Whatsapp</i></th>
+                                    <td>0821 9955 8762</td>
+                                </tr>
+                                <tr>
+                                    <th><i class="fa fa-phone"> Phone</i></th>
+                                    <td>0819 1259 4392</td>
+                                </tr>
+                                <tr>
+                                    <th><i class="fa fa-facebook">  Facebook</i></th>
+                                    <td>Deo Karmel Deki</td>
+                                </tr>
+                                <tr>
+                                    <th><i class="fa fa-youtube">  YouTube</i></th>
+                                    <td>Siansahan 64</td>
+                                </tr>
+                                <tr>
+                                    <th><i class="fa fa-twitter">  Twitter</i></th>
+                                    <td>@deky08961833</td>
+                                </tr>
+                                <tr>
+                                    <th>Jenis Kelamin</th>
+                                    <td>Laki-laki</td>
+                                </tr>
+                                <tr>
+                                    <th>Hobi</th>
+                                    <td>- Game <br>
+                                        - Tidur</td>>
+                                </tr>
+                                <tr>
+                                    <th>Golongan Darah</th>
+                                    <td>-</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/js/bootstrap.min.js" integrity="sha384-3qaqj0lc6sV/qpzrc1N5DC6i1VRn/HyX4qdPaiEFbn54VjQBEU341pvjz7Dv3n6P" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="script.js"></script>
+  </body>
 </html>
